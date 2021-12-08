0631ac4a9ab087607a222909a939b4b481a30e43
Raphael Gundim <raphael.gundim@gmail.com>
2021-12-08 02:15:01 -0300

    start projetct

---
+++ src/config/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    developement:{
+        database:{
+            host:'localhost',
+            port:3306,
+            name: 'cursoNodeMySql',
+            dialect: 'mysql',
+            user: 'root',
+            password:'Juli@16072@21'
+
+        }
+    },
+    production: {
+        database: {
+            host: process.env.DB_HOST,
+            host: process.env.DB_PORT
+        }
+    }
+}
